6adfdc40e290ca0a1ed73c3996cb296fc28f9bb7
Anand Sharma <anand.sharma@autovyn.in>
2026-01-16 03:14:08 +0530

    Added SafeAreaView Insets

--- App.jsx
+++ App.jsx
@@ -1,24 +1,35 @@
-import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import Router from './src/navigation'
 import Store from '@store';
 import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={'light-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
+const App = () => {
   return (
     <Provider store={Store}>
-      <Router />
+      <SafeAreaProvider>
+        <StatusBar barStyle={'dark-content'} />
+        <AppContent />
+      </SafeAreaProvider>
     </Provider>
   );
 }
+
+const AppContent = () => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        flex: 1,
+      }}
+    >
+      <Router />
+    </View>
+  );
+};
 
 export default App;
