8209be2ff737bb727a5c699177c4f5eb688fb8ea
yogendra Joshi <yogendra.joshi@autovyn.in>
2026-01-16 01:14:18 +0530

    loginScreen

--- src/screens/login/index.jsx
+++ src/screens/login/index.jsx
@@ -1,43 +1,68 @@
-import { StyleSheet, Text, TextInput, View } from "react-native"
-import { Fonts } from '../../utills'
 import { useState } from "react"
-
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
 const Login = () => {
     const [state, setState] = useState({
-        inputValue: ''
-    });
-    const handleChange =t=> {}
-    const handlePress =t=> {}
+        mobileNumber: "",
+        password: ''
+    })
+
+    const handleMobileNum = (text) => {
+        const numericValue = text.replace(/[^0-9]/g, "")
+        setState(prev => ({ ...prev, mobileNumber: numericValue }))
+    };
+
+    const handlePassword = (text) => {
+        setState(prev => ({ ...prev, password: text }))
+    };
+
     return (
-        <View style={{ backgroundColor: 'black', flex: 1 }}>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-                <Text style={{ color: '#00c0fa', fontSize: 40 }}>TATA MOTORS</Text>
-                <Text style={{ color: 'white' }}>Dealer Vehicle Pickup</Text>
-            </View>
-            <View>
-                <Text>Mobile Number</Text>
-                <TextInput
-                    style={styles.otpTextInputStyle}
-                    placeholder="MSPIN"
-                    placeholderTextColor={"grey"}
-                    keyboardType='numeric'
-                    value={state?.inputValue}
-                    onChangeText={handleChange}
-                    onSubmitEditing={handlePress}
-                />
-            </View>
-        </View>
+        // <View style={{ backgroundColor: 'black', flex: 1, justifyContent: 'center' }}>
+            <KeyboardAvoidingView style={{ backgroundColor: 'black', flex: 1, justifyContent: 'center' }} behavior={Platform.OS === 'ios' ? 'padding' : null}>
+
+                <View style={{ alignItems: 'center', justifyContent: 'center', paddingVertical: 10 }}>
+                    <Text style={{ color: '#00c0fa', fontSize: 40, fontWeight: 'bold' }}>TATA MOTORS</Text>
+                    <Text style={{ color: '#848484', fontSize: 20 }}>Dealer Vehicle Pickup</Text>
+                </View>
+                <View style={{ paddingHorizontal: 25 }}>
+                    <Text style={{ color: '#848484', paddingVertical: 7 }}>Mobile Number</Text>
+                    <TextInput
+                        style={styles.otpTextInputStyle}
+                        placeholder="Enter your 10-digit mobile number"
+                        placeholderTextColor={"grey"}
+                        keyboardType='numeric'
+                        value={state?.mobileNumber}
+                        onChangeText={handleMobileNum}
+                    />
+                </View>
+                <View style={{ paddingHorizontal: 25 }}>
+                    <Text style={{ color: '#848484', paddingVertical: 7 }}>Password</Text>
+                    <TextInput
+                        style={styles.otpTextInputStyle}
+                        placeholder="Enter your Password"
+                        placeholderTextColor={"grey"}
+                        value={state?.password}
+                        onChangeText={handlePassword}
+                    // onSubmitEditing={handlePress}
+                    />
+                </View>
+                <View style={{ paddingHorizontal: 25, paddingVertical: 20 }}>
+                    <TouchableOpacity style={{ backgroundColor: '#252525', borderRadius: 7, padding: 10, alignItems: 'center' }}>
+                        <Text style={{ color: '#848484', }}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
+        // </View>
     )
 }
 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
     otpTextInputStyle: {
         fontSize: 17,
-        flex: 1,
-        color: 'black',
-        fontFamily: Fonts.themeFontBold,
+        color: '#848484',
+        backgroundColor: '#1a1a1a',
+        borderRadius: 7
     },
 })
 
